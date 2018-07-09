9d72cea6768cc9971563a60d105cb2c96436354a
yanzehao <184994471@qq.com>
2018-07-09 21:35:21 +0800

    jj

--- js/task02-04/js/task234-02.js
+++ js/task02-04/js/task234-02.js
@@ -31,37 +31,42 @@ for(i=0;i<num;i++){
 }
 document.getElementById('box').innerHTML=box.join("")
 console.log(box);
+
 //点击玩家角色事件
 $('.box-name').click(function(){
   //获取当前选中的玩家索引
   index = $(".box-name").index(this);
-  sessionStorage.setItem("pnum",index);
   console.log("当前玩家索引:"+index);
   if (text1[index]=="杀手"){
     alert("不能搞自己人啊!");
   }
   else if(text1[index]=="平民"){
-    //先重置背景颜色
-    $('.box-name').css("background","#f5c97b");
-    //再指定当前点击的div的背景颜色
-    $(this).css("background","#999999");
+
     //本地存储/存入变量/选取被杀死的平民编号
     sessionStorage.setItem("key3", index+1);
     console.log('杀死平民:'+(index+1));
-    //获取平民索引数组
+    //获取平民索引数组(初始))
     var pingstr=sessionStorage.getItem("ping");
     var ping = JSON.parse(pingstr);
     console.log(ping);
-    for (i=0;i<day;i++){
-      if(index==ping[i])
-      //删除平民玩家索引数组中的索引元素
-      ping.splice($.inArray(index,ping),1);
+    for(var i=0;i<day;i++){
+      //死亡平民数组
+      var dead = [];
+      dead.push(index);
+      console.log(dead);
+      //存活平民数组
+      ping.splice(index,1);
       console.log(ping);
-      console.log(ping.length);
-      // if(ping.length==0){
-      //   window.location.href = "../html/task234-03.html"
+      //先重置背景颜色
+      $('.box-name').eq(ping[]).css("background","#f5c97b");
+      //再指定当前点击的div的背景颜色
+      $(this).css("background","#999999");
     }
-    // }
+    // ping.splice($.inArray(index,ping),1);
+    // console.log(ping);
+    // console.log(ping.length);
+    // if(ping.length==0){
+    //   window.location.href = "../html/task234-03.html"
 
     // //删除平民玩家的索引
     // ping.splice(index,1)
