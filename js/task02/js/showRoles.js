174e37724bcb91800fb100c3a3a0a76c0b92c8fe
yanzehao <184994471@qq.com>
2018-07-15 01:01:07 +0800

    s's
    ss

--- js/task02/js/showRoles.js
+++ js/task02/js/showRoles.js
@@ -19,7 +19,7 @@ $(".footer").click(function(){
 		if(i%2==0){ //点击次数为偶数时
 			$(".index").html(Math.ceil((1/2)*(i+1)));
 			$(".bg").html(null);	
-			$(".bg").prepend('<img src="img/hidden.png" style="width: 68%;padding-top: 16rem">');
+			$(".bg").prepend('<img src="img/hidden.png" style="width: 68%;padding-top: 8rem">');
 			$(".footer span").text("查看"+Math.ceil((1/2)*(i+2))+"号身份");
 		}
 		else{ //点击次数为奇数时
@@ -28,10 +28,10 @@ $(".footer").click(function(){
 			$(".bg").prepend('<img src="img/flop.png">');
 			// $(".bg img").css({"width":"50%","padding-top":"13rem"});
 			if(all[Math.ceil((1/2)*i)-1]==1){
-				$(".bg img").after('<div style="padding-top: 5rem; font-size: 6rem; color: #29bde0">角色：杀手</div>');
+				$(".bg img").after('<div style="padding-top: .5rem; font-size: 3rem; color: #29bde0">角色：杀手</div>');
 			}
 			else{
-				$(".bg img").after('<div style="padding-top: 5rem; font-size: 6rem; color: #29bde0">角色：平民</div>');
+				$(".bg img").after('<div style="padding-top: .5rem; font-size: 3rem; color: #29bde0">角色：平民</div>');
 			}
 			$(".footer span").text("隐藏并传递给"+Math.ceil((1/2)*(i+2))+"号");
 		}
@@ -41,10 +41,10 @@ $(".footer").click(function(){
 		$(".bg").html(null);
 		$(".bg").prepend('<img src="img/flop.png">');
 		if(all[Math.ceil((1/2)*i)-1]==1){
-			$(".bg img").after('<div style="padding-top: 5rem; font-size: 6rem; color: #29bde0">角色：杀手</div>');
+			$(".bg img").after('<div style="padding-top: .5rem; font-size: 3rem; color: #29bde0">角色：杀手</div>');
 		}
 		else{
-			$(".bg img").after('<div style="padding-top: 5rem; font-size: 6rem; color: #29bde0">角色：平民</div>');
+			$(".bg img").after('<div style="padding-top: .5rem; font-size: 3rem; color: #29bde0">角色：平民</div>');
 		}
 		$(".footer span").text("法官查看");
 	}
